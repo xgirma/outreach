9dbff1ac50154638e715107e82585c058a46347e
xgirma <xgirma@gmail.com>
2018-05-03 21:39:00 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <p className="App">admin board</p>
+    );
+  }
+}
+
+export default App;
