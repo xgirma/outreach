9eb451c4618de36610b67a8728b13fe6b1066b73
xgirma <xgirma@gmail.com>
2018-05-06 17:01:46 -0700

    init server app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,22 @@
+import http from 'http';
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
-const app = express();
+import config from './config'
+import routes from './routes'
+
+let app = express();
+app.server = http.create(app);
+
+// middleware
+
+// passport config
+
+// api routes v1
+app.use('/v1', routes);
+
+app.server.listen(config.port);
+console.log('Stared on', ${app.server.address().port});
+
+export default app;
