7b32d54d405a97503626f1c2f6d2b5d19a5d0865
xgirma <xgirma@gmail.com>
2018-05-22 19:24:33 -0700

    update 404 handling

--- backend/src/api/restRouter.js
+++ backend/src/api/restRouter.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import { infoRouter } from './resources/info';
-import { apiErrorHandler } from './modules/errorHandler';
 
 export const restRouter = express.Router();
 
 restRouter.use('/info', infoRouter);
-restRouter.use(apiErrorHandler);
