dc711e1d91ff6c52f25a65c4ad28d1217ef37f5c
xgirma <xgirma@gmail.com>
2018-05-27 05:28:29 -0700

    add test for service api

--- backend/__tests__/service.spec.js
+++ backend/__tests__/service.spec.js
@@ -17,46 +17,46 @@ describe('service', () => {
       .request(url)
       .post('/service')
       .send(data);
-    
+
     created(result);
   });
-  
+
   test('GET /service', async () => {
     const result = await chai.request(url).get('/service');
-    
+
     ok(result);
   });
-  
+
   test('GET /service/{id}', async () => {
     const result = await chai.request(url).get(`/service/${data._id}`);
-    
+
     ok(result);
   });
-  
+
   test('GET /service/{id}: non existing', async () => {
     const result = await chai.request(url).get(`/service/${badId}`);
-    
+
     notFound(result);
   });
-  
+
   test('PUT /service/{id}', async () => {
     const result = await chai
       .request(url)
       .put(`/service/${data._id}`)
       .send(churchService());
-    
+
     created(result);
   });
-  
+
   test('DELETE /service/{id}', async () => {
     const result = await chai.request(url).delete(`/service/${data._id}`);
-    
+
     created(result);
   });
-  
+
   test('DELETE /service/{id}: non existing', async () => {
     const result = await chai.request(url).delete(`/service/${badId}`);
-    
+
     notFound(result);
   });
 });
