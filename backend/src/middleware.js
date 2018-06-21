45cc5cdc52fbe0c67c616f1b37f01f83f29e6c9a
xgirma <xgirma@gmail.com>
2018-06-21 00:37:39 -0700

    method-override

--- backend/src/middleware.js
+++ backend/src/middleware.js
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
 import morgan from 'morgan';
 
 const setGlobalMiddleware = (app) => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json({ limit: process.env.BODY_LIMIT }));
+  app.use(methodOverride('X-HTTP-Method-Override'));
   app.use(morgan('combined'));
 };
 
