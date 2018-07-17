3c1765098396917ee7d4913e2c97355af97f3022
xgirma <xgirma@gmail.com>
2018-07-17 01:10:39 -0700

    update event model

--- backend/src/api/resources/event/event.model.js
+++ backend/src/api/resources/event/event.model.js
@@ -2,34 +2,32 @@ import mongoose from 'mongoose';
 import { isEmail } from 'validator';
 
 export const schema = {
-  _id: {
-    type: Number,
-    default: 202020,
-  },
   am: {
-    title: { type: String, maxlength: 200, required: [true, 'Event must have a title'] },
+    title: { type: String, maxlength: 200, required: [true, 'event must have a title'] },
     description: { type: String },
   },
   en: {
-    title: { type: String, maxlength: 200, required: [true, 'Event must have a title'] },
+    title: { type: String, maxlength: 200, required: [true, 'event must have a title'] },
     description: { type: String },
   },
   address: {
-    street: { type: String, required: [true, 'Event must have a street'], maxlength: 200 },
-    city: { type: String, required: [true, 'Event must have a city'], maxlength: 50 },
+    street: { type: String, required: [true, 'event must have a street'], maxlength: 200 },
+    city: { type: String, required: [true, 'event must have a city'], maxlength: 50 },
     state: { type: String, maxlength: 50 },
     zip: { type: String, maxlength: 50 },
-    country: { type: String, required: [true, 'Event must have a country'], maxlength: 100 },
+    country: { type: String, required: [true, 'event must have a country'], maxlength: 100 },
   },
   email: {
     type: String,
     trim: true,
     lowercase: true,
-    validate: [isEmail, 'Invalid email'],
+    validate: [isEmail, 'invalid email'],
   },
-  phone: { type: String, required: [true, 'Event must have a phone'] },
+  phone: { type: String, required: [true, 'event must have a phone'] },
   date_start: { type: Date, default: Date.now, index: true },
   date_end: { type: Date, default: Date.now, index: true },
+  adminname: { type: String, required: [true, 'event must have a adminname'], maxlength: 20 },
+  date: { type: Date, default: Date.now, index: true }, // document creation date
 };
 
 const eventSchema = new mongoose.Schema(schema);
