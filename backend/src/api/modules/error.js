75a25c8c77b25d12438f29b2669bc82709353a94
xgirma <xgirma@gmail.com>
2018-07-29 13:50:01 -0700

    fix error

--- backend/src/api/modules/error.js
+++ backend/src/api/modules/error.js
@@ -4,46 +4,40 @@ export const UNAUTHORIZED =
   'the request has not been applied because it lacks valid authentication credentials for the target resource.';
 const BAD_REQUEST =
   'the server cannot or will not process the request due to something that is perceived to be a client error';
-const WEAK_PASSWORD = 'weak password based on the OWASP password strength test';
-
 export const FORBIDDEN =
   'the request has not been applied because it lacks valid authentication credentials for the target resource.';
+const WEAK_PASSWORD = 'weak password based on the OWASP password strength test';
 
-const WeakPassword = (msg) => {
-  const message = msg || WEAK_PASSWORD;
-  const err = new Error(message);
+const WeakPassword = (msg = WEAK_PASSWORD) => {
+  const err = new Error(msg);
   err.name = WeakPassword.name;
   err.status = 400;
   return err;
 };
 
-const BadRequest = (msg) => {
-  const message = msg || BAD_REQUEST;
-  const err = new Error(message);
+const BadRequest = (msg = BAD_REQUEST) => {
+  const err = new Error(msg);
   err.name = BadRequest.name;
   err.status = 400;
   return err;
 };
 
-const Unauthorized = (msg) => {
-  const message = msg || UNAUTHORIZED;
-  const err = new Error(message);
+const Unauthorized = (msg = UNAUTHORIZED) => {
+  const err = new Error(msg);
   err.name = Unauthorized.name;
   err.status = 401;
   return err;
 };
 
 const Forbidden = (msg = FORBIDDEN) => {
-  const message = msg;
-  const err = new Error(message);
+  const err = new Error(msg);
   err.name = Forbidden.name;
   err.status = 403;
   return err;
 };
 
-const NotFound = (msg) => {
-  const message = msg || RESOURCE_NOT_FOUND;
-  const err = new Error(message);
+const NotFound = (msg = RESOURCE_NOT_FOUND) => {
+  const err = new Error(msg);
   err.name = NotFound.name;
   err.status = 404;
   return err;
