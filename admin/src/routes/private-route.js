8c1ba9e245b1279b9079a228091883ceddb903da
xgirma <xgirma@gmail.com>
2018-08-27 16:12:20 -0700

    add signin

--- admin/src/routes/private-route.js
+++ admin/src/routes/private-route.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={props => (
-    localStorage.getItem('username')
+    localStorage.getItem('username') && localStorage.getItem('token')
       ? <Component {...props} />
       : <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
   )} />
