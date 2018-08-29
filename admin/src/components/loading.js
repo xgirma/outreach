8da1b0d2240524b9f2a7d1010f0e16ac4c25e822
xgirma <xgirma@gmail.com>
2018-08-28 19:35:29 -0700

    add Loading component

---
+++ admin/src/components/loading.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/destructuring-assignment, no-unused-expressions */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Loading extends Component {
+  static displayName = 'loading';
+
+  static propTypes = {
+    text: PropTypes.string,
+    speed: PropTypes.number,
+  };
+
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300,
+  };
+
+  state = {
+    text: this.props.text,
+    speed: this.props.speed,
+  };
+
+  componentDidMount() {
+    const { text, speed } = this.state;
+    const stopper = `${text}...`;
+
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState({ text: this.props.text })
+        : this.setState((currentState) => ({ text: `${currentState.text}.` }));
+    }, speed);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
+  render() {
+    const { text } = this.state;
+    return <p>{text}</p>;
+  }
+}
+
+export default Loading;
