7b4a808b04f8b4e3a4da071eecaa497c0da67667
xgirma <xgirma@gmail.com>
2018-08-31 11:20:49 -0700

    add event reducer and container

--- admin/src/reducers/index.js
+++ admin/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 
 import alert from './alert';
+import event from './event';
 import authentication from './signin';
 import introduction from './introduction';
 import navigation from './navigation';
 
 export default combineReducers({
   alert,
+  event,
   authentication,
   introduction,
   navigation,
