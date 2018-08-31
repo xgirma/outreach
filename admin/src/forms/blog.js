cf0a0d3702f6e342ff1d547ab5202b06e1ccdab4
xgirma <xgirma@gmail.com>
2018-08-31 15:20:36 -0700

    add blog

--- admin/src/forms/blog.js
+++ admin/src/forms/blog.js
@@ -84,8 +84,260 @@ class BlogForm extends Component {
     }
   }
   
+  onAmEditorChange = (amharic) => {
+    const description = amharic.toString('html');
+    this.setState((prevState) => ({
+      ...prevState,
+      amharic,
+      item: {
+        ...prevState.item,
+        am: {
+          ...prevState.item.am,
+          description,
+        },
+      },
+    }));
+  };
+  
+  onEnEditorChange = (english) => {
+    const description = english.toString('html');
+    this.setState((prevState) => ({
+      ...prevState,
+      english,
+      item: {
+        ...prevState.item,
+        en: {
+          ...prevState.item.en,
+          description,
+        },
+      },
+    }));
+  };
+  
+  handleAmharicInput = (event) => {
+    const { value, name } = event.target;
+    this.setState((prevState) => ({
+      ...prevState,
+      item: {
+        ...prevState.item,
+        am: { ...prevState.item.am, [name]: value },
+      },
+    }));
+  };
+  
+  handleEnglishInput = (event) => {
+    const { value, name } = event.target;
+    this.setState((prevState) => ({
+      ...prevState,
+      item: {
+        ...prevState.item,
+        en: { ...prevState.item.en, [name]: value },
+      },
+    }));
+  };
+  
+  handleItemInput = (event) => {
+    const { value, name } = event.target;
+    this.setState((prevState) => ({
+      ...prevState,
+      item: {
+        ...prevState.item,
+        [name]: value,
+      },
+    }));
+  };
+  
+  handleFormClear = (event) => {
+    event.preventDefault();
+    this.setState({
+      item: blankItem,
+      add: true,
+      amharic: createValueFromString('', 'html'),
+      english: createValueFromString('', 'html'),
+    });
+  };
+  
+  handleEdit = (item) => {
+    const amharicHtml = item.am.description;
+    const englishHtml = item.en.description;
+    this.setState({
+      item,
+      add: false,
+      amharic: createValueFromString(amharicHtml, 'html'),
+      english: createValueFromString(englishHtml, 'html'),
+    });
+  };
+  
+  handleFormUpdate = async (event) => {
+    event.preventDefault();
+    const { updateBlog, getBlog, addBlog } = this.props;
+    
+    const result = this.state.add
+      ? await addBlog(this.state.item)
+      : await updateBlog(this.state.item);
+    
+    const { status, data } = result;
+    if (status === 'success') {
+      const newResult = await getBlog();
+      if (newResult.status === 'success' && newResult.data.length > 0) {
+        const amharicHtml = newResult.data[0].am.description;
+        const englishHtml = newResult.data[0].en.description;
+        this.setState({
+          items: newResult.data,
+          item: newResult.data[0],
+          error: blankError,
+          amharic: createValueFromString(amharicHtml, 'html'),
+          english: createValueFromString(englishHtml, 'html'),
+        });
+      }
+      
+      if (newResult.status === 'fail' || status === 'error') {
+        this.setState({
+          error: { ...data },
+        });
+      }
+    }
+    
+    if (status === 'fail' || status === 'error') {
+      this.setState({
+        error: { ...data },
+      });
+    }
+  };
+  
+  handleDelete = async (id) => {
+    const { deleteBlog, getBlog } = this.props;
+    const result = await deleteBlog(id);
+    const { status, data } = result;
+    if (status === 'success') {
+      const newResult = await getBlog();
+      if (newResult.status === 'success' && newResult.data.length > 0) {
+        const amharicHtml = newResult.data[0].am.description;
+        const englishHtml = newResult.data[0].en.description;
+        this.setState({
+          items: newResult.data,
+          item: newResult.data[0],
+          error: blankError,
+          amharic: createValueFromString(amharicHtml, 'html'),
+          english: createValueFromString(englishHtml, 'html'),
+        });
+      }
+      
+      if (newResult.status === 'success' && newResult.data.length === 0) {
+        this.setState({
+          items: newResult.data,
+          item: blankItem,
+          error: blankError,
+          amharic: createValueFromString('', 'html'),
+          english: createValueFromString('', 'html'),
+        });
+      }
+      
+      if (newResult.status === 'fail' || status === 'error') {
+        this.setState({
+          error: { ...data },
+        });
+      }
+    }
+    
+    if (status === 'fail' || status === 'error') {
+      this.setState({
+        error: { ...data },
+      });
+    }
+  };
+  
   render() {
-    return <div>Hello blog</div>;
+    return (
+      <div>
+          <div>
+            {this.state.error.name !== '' &&
+            `Name: ${this.state.error.name} Message: ${this.state.error.message}`}
+          </div>
+        <form onSubmit={this.handleSubmit}>
+          {/* amharic */}
+          <Input
+            type="text"
+            title="Title"
+            name="title"
+            value={this.state.item.am.title}
+            placeholder="Enter your blog title"
+            onChange={this.handleAmharicInput}
+          />
+          <label>Description</label>
+          <RichTextEditor
+            value={this.state.amharic}
+            onChange={this.onAmEditorChange}
+            toolbarConfig={toolbarConfig}
+          />
+          {/* english */}
+          <Input
+            type="text"
+            title="Title"
+            name="title"
+            value={this.state.item.en.title}
+            placeholder="Enter your service title"
+            onChange={this.handleEnglishInput}
+          />
+          <label>Description</label>
+          <RichTextEditor
+            value={this.state.english}
+            onChange={this.onEnEditorChange}
+            toolbarConfig={toolbarConfig}
+          />
+          {/* author, date, tags */}
+          <label>author, date, tags</label>
+          <Input
+            type="text"
+            title="Author"
+            name="author"
+            value={this.state.item.author}
+            placeholder="Enter author name"
+            onChange={this.handleItemInput}
+          />
+          <Input
+            type="text"
+            title="Start date"
+            name="dateStart"
+            value={this.state.item.dateStart}
+            placeholder="Enter your blog publication date, it can be future date"
+            onChange={this.handleItemInput}
+          />
+          <Input
+            type="text"
+            title="Tags"
+            name="tag"
+            value={this.state.item.tag.toString()}
+            placeholder="Enter your blog tags coma separated date"
+            onChange={this.handleItemInput}
+          />
+          {/* clear, submit */}
+          <Button action={this.handleFormClear} title="Clear" />
+          <Button action={this.handleFormUpdate} title="Submit" />
+        </form>
+        <table>
+          <thead>
+          <tr>
+            <th>Created on</th>
+            <th>By</th>
+            <th>Title</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+          </thead>
+          <tbody>
+          {this.state.items.map((item) => (
+            <TableRow
+              key={item._id}
+              item={item}
+              onDelete={this.handleDelete}
+              onEdit={this.handleEdit}
+            />
+          ))}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
